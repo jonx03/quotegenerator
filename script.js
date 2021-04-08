0e87e57c3b52a443291d373a3815d0e55ce7718f
Jonathan Rocha <77184572+jonx03@users.noreply.github.com>
2021-04-07 22:13:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,62 +3,126 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const simpsonsQuoteBtn = document.getElementById('simpson-quote');
+const imgSimpsonCharacter = document.getElementById('simpson-character');
+let quote = "";
+let author = "";
+
+//custom server to avoid  CORS issue
+const proxyUrl = 'https://dry-cove-58467.herokuapp.com/';
+const apiUrl =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+const simpsonsQuoteUrl = 'https://thesimpsonsquoteapi.glitch.me/quotes';
+
+
 const loader = document.getElementById('loader');
+var errorTimeOut = 0;
 
-// Loading Spinner Shown
-function loading() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
-// Remove Loading Spinner
-function complete() {
-  if (!loader.hidden) {
-    quoteContainer.hidden = false;
-    loader.hidden = true;
-  }
-}
-
-// Get Quote From API
-async function getQuote() {
-  loading();
-  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-  try {
-    const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    // Check if Author field is blank and replace it with 'Unknown'
-    if (data.quoteAuthor === '') {
-      authorText.innerText = 'Unknown';
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
     } else {
-      authorText.innerText = data.quoteAuthor;
+
     }
-    // Dynamically reduce font size for long quotes
-    if (data.quoteText.length > 120) {
-      quoteText.classList.add('long-quote');
-    } else {
-      quoteText.classList.remove('long-quote');
-    }
-    quoteText.innerText = data.quoteText;
-    // Stop Loading, Show Quote
-    complete();
-  } catch (error) {
-    getQuote();
-  }
 }
 
-// Tweet Quote
+function showCharacterImage(imageUrl) {
+    if (imageUrl) {
+        imgSimpsonCharacter.hidden = false;
+        imgSimpsonCharacter.src = imageUrl;
+    } else {
+        hideCharacterImage();
+    }
+}
+
+function hideCharacterImage() {
+    imgSimpsonCharacter.hidden = true;
+}
+
+
+async function getQuote(quoteType) {
+
+    showLoadingSpinner();
+    hideCharacterImage();
+
+    try {
+
+        let url = proxyUrl + apiUrl;
+        if (quoteType === 'simpsons quote') {
+            url = simpsonsQuoteUrl;
+
+        }
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        //check if simpsons quote requested
+        if (quoteType === 'simpsons quote') {
+            quote = data[0].quote;
+            author = data[0].character;
+            showCharacterImage(data[0].image);
+
+        } else { //default quote API
+            quote = data.quoteText;
+            author = data.quoteAuthor;
+
+        }
+        //If response has no author , reset to 'unknown'
+        if (author === '') {
+            author = 'Unknown';
+        }
+        authorText.innerText = author;
+
+        //Dynamically Reduce quote font size for long quote
+        const maxQuoteLength = 80;
+        if (quote.length > maxQuoteLength) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = quote;
+
+        removeLoadingSpinner();
+
+
+    } catch (error) {
+        //counter to avoid infinite recursive function call threshhold set to 30
+        const errorThreshhold = 50;
+        if (errorTimeOut <= errorThreshhold) {
+            getQuote();
+
+        } else {
+            quoteText.innerText = "Oops...Sorry. There was an error. Please try again later."
+        }
+        errorTimeOut += 1;
+
+        console.log(" Error fetching quote: " + error);
+    }
+}
+
+
 function tweetQuote() {
-  const quote = quoteText.innerText;
-  const author = authorText.innerText;
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-  window.open(twitterUrl, '_blank');
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+
 }
 
-// Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
+//Add Event Listeners to New Quote and Tweet buttons
+newQuoteBtn.addEventListener('click', () => {
+    getQuote('new quote')
+});
+simpsonsQuoteBtn.addEventListener('click', () => {
+    getQuote('simpsons quote')
+});
 twitterBtn.addEventListener('click', tweetQuote);
 
-// On Load
-getQuote();
+//call getQuote on document Load 
+getQuote('new quote');
